99112960cd4e6c628189d2928a6c3c6a891b98bd
marvel6 <marvelloussolomon49@gmail.com>
2023-02-28 02:23:47 +0100

    new commit

--- controller/user.js
+++ controller/user.js
@@ -1,58 +1,76 @@
+const User = require('../model/user')
+
 
 module.exports = {
-    login: (req,res) => {
+    login: async (req, res) => {
 
         res.render("login")
 
     },
 
-    register: (req,res) => {
-      res.render("register")
+    register: async (req, res) => {
+        res.render("register")
     },
 
-    submitLogin :(req,res) => {
-       console.log(req.body)
+    submitLogin: async (req, res) => {
+        console.log(req.body)
 
-       res.send('hello')
+        res.send('hello')
     },
 
-    submitRegister : (req,res) => {
-    
-        const {name,email,password,password2} = req.body
-        
+    submitRegister: async (req, res) => {
+
+        const { name, email, password, password2 } = req.body
+
         let errors = [];
 
-        if(!name || !email || !password || !password2){
+        if (!name || !email || !password || !password2) {
 
-            errors.push({msg:'Please provide values for missing fields'})
+            errors.push({ msg: 'Please provide values for missing fields' })
 
         }
 
 
-        if(password !== password2){
-            errors.push({msg:'Password mismatch please provide valid result'})
+        if (password !== password2) {
+            errors.push({ msg: 'Password mismatch please provide valid result' })
         }
 
 
-        if(password.length < 6){
-            errors.push({msg:'password must not be less than 6 characters'})
+        if (password.length < 6) {
+            errors.push({ msg: 'password must not be less than 6 characters' })
         }
 
-        if(errors.length > 0){
-            res.render('register',{
+        if (errors.length > 0) {
+            res.render('register', {
                 errors,
                 name,
                 email,
                 password,
                 password2
             })
-        }else{
-
-            res.send('hello')
-
         }
 
-       
+        const user = await User.findOne({ email })
+        
+        if (user) {
+            res.render('register', { errors, name, email, password })
+        }
+
+        const newUser = {
+            username: name,
+            email,
+            password
+        }
+
+        await User.create(newUser)
+
+        setTimeout(() => {
+            res.render('login')
+        }, 2000)
+
+
+
+
 
     }
 }
--- model/user.js
+++ model/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userSchema = mongoose.Schema({
     username:{
@@ -20,6 +21,16 @@ const userSchema = mongoose.Schema({
 
 })
 
+
+userSchema.pre('save', async function () {
+  
+    if (!this.isModified('password')) return;
+  
+    const salt = await bcrypt.genSalt(10);
+    
+    this.password = await bcrypt.hash(this.password, salt);
+  });
+  
 
 
 module.exports = mongoose.model('User',userSchema)
