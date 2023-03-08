019969522e5e1f6ddd4a8d8428199255b84aff87
marvel6 <marvelloussolomon49@gmail.com>
2023-03-08 12:59:08 +0100

    new changes

--- config/googleAauth.js
+++ config/googleAauth.js
@@ -3,7 +3,12 @@ const userGoolge = require('../model/google')
 const User = require('../model/user')
 
 module.exports = async (passport) => {
-   
+    passport.use(new googleStrategy({
+        // clientID:process.env.GOOGLE_CLIENT_ID,
+        // clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+        // callbackURL:'/auth/google/callback'
+
+    }))
 
 
 
